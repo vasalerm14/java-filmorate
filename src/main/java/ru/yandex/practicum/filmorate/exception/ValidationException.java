9485a7ceb5dbb1499be49d63a3fc98a1b1ed80bf
vasalerm1401 <vasalerm1401@yandex.ru>
2024-02-20 01:50:47 +0300

    Добавлены User и Film, и контроллеры к ним

--- src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
@@ -1,7 +1,7 @@
 package ru.yandex.practicum.filmorate.exception;
 
-public class ValidationException  extends RuntimeException{
-    public ValidationException(String msg){
+public class ValidationException extends RuntimeException {
+    public ValidationException(String msg) {
         super(msg);
     }
 }
