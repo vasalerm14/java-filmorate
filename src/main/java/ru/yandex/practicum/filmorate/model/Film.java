de609c734b0e69a8ec08c5820165e4cedb8ba095
vasalerm14 <134870894+vasalerm14@users.noreply.github.com>
2024-02-20 01:57:51 +0300

    Revert "Добавлены User и Film, и контроллеры к ним"

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,24 +1,12 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Data;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Positive;
-import javax.validation.constraints.Size;
-import java.time.LocalDate;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * Film.
  */
-@Data
+@Getter
+@Setter
 public class Film {
-    private Integer id;
-
-    @NotBlank
-    private String name;
-    @Size(min = 0, max = 200)
-    private String description;
-    private LocalDate releaseDate;
-    @Positive
-    private int duration;
 }
