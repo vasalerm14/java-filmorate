9504345fcb6fb1e2b6a408d11a4a103f3e34824b
vasalerm1401 <vasalerm008@gmail.com>
2024-03-08 22:35:44 +0300

    Правки в CodeStyle

--- src/main/java/ru/yandex/practicum/filmorate/exceptionHandler/ErrorResponse.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptionHandler/ErrorResponse.java
@@ -8,7 +8,7 @@ public class ErrorResponse {
         this.errorMessage = errorMessage;
     }
 
-    public String getErrorMessage(){
+    public String getErrorMessage() {
         return this.errorMessage;
     }
 
