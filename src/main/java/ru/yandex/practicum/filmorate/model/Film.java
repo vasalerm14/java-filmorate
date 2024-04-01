c14e2f2ca550330d466c03c7c6cb4558768f9512
vasalerm1401 <vasalerm008@gmail.com>
2024-04-01 23:41:06 +0300

    Правки в CodeStyle

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -8,9 +8,7 @@ import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.LinkedHashSet;
-import java.util.Set;
 
 @Data
 public class Film {
